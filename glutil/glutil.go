7f946e3831028f647b0c24d01ad14ba479cd78f2
tstraus <tckrsstrsbgh@gmail.com>
2017-11-04 16:18:11 -0400

    drawing two triangles

--- glutil/glutil.go
+++ glutil/glutil.go
@@ -2,13 +2,15 @@ package glutil
 
 import (
 	"runtime"
+	"strings"
+	"fmt"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
 // InitGL initializes GLFW and OpenGL
-func InitGL(width, height int) (*glfw.Window, uint32) {
+func InitGL(width, height int, vertexShaderSource string, fragmentShaderSource string) (*glfw.Window, uint32) {
 	runtime.LockOSThread()
 
 	err := glfw.Init()
@@ -33,8 +35,53 @@ func InitGL(width, height int) (*glfw.Window, uint32) {
 		panic(err)
 	}
 
+	vertexShader, _ := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	fragmentShader, _ := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+
 	program := gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
 	gl.LinkProgram(program)
 
 	return window, program
+}
+
+// MakeVao initializes and returns a vertex array from the points provided
+func MakeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4 * len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength + 1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
 }
--- gogl.go
+++ gogl.go
@@ -12,22 +12,61 @@ const (
 	h = 480
 )
 
-func draw(window *glfw.Window, program uint32) {
+var (
+	triangle = []float32 {
+		-0.25, 0.5, 0, // top
+		-0.75, -0.5, 0, // left
+		0.25, -0.5, 0, // right
+	}
+
+	triangle2 = []float32 {
+		0.25, -0.5, 0, // bottom
+		-0.25, 0.5, 0, // left
+		0.75, 0.5, 0, // right
+	}
+
+	vertexShaderSource = `
+		#version 410
+		in vec3 vp;
+		void main() {
+			gl_Position = vec4(vp, 1.0);
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 410
+		out vec4 frag_colour;
+		void main() {
+			frag_colour = vec4(1, 1, 1, 1);
+		}
+	` + "\x00"
+)
+
+func draw(objects []uint32, window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
+
+	for _, object := range objects {
+		gl.BindVertexArray(object)
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle) / 3))
+	}
 
 	window.SwapBuffers()
 	glfw.PollEvents()
 }
 
 func main() {
-	window, program := glutil.InitGL(w, h)
+	window, program := glutil.InitGL(w, h, vertexShaderSource, fragmentShaderSource)
 	defer glfw.Terminate() // wait for init
 
 	gl.ClearColor(0, 0.5, 1.0, 1.0)
 
+	var objects []uint32
+	objects = append(objects, glutil.MakeVao(triangle))
+	objects = append(objects, glutil.MakeVao(triangle2))
+
 	for !window.ShouldClose() {
-		draw(window, program)
+		draw(objects, window, program)
 	}
 }
 
